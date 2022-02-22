4e922288b451f17ba0ba665d99d2e30204f499de
Isaac <isaacwildsecond@gmail.com>
2022-02-22 13:37:44 +0000

    completed all tasks

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string,number) {
 
+    let repeated = "";
+    if(number < 0){
+        return "ERROR";
+    }else
+    for (let i = 0; i<number; i++){
+            repeated = repeated + string;
+        }
+    
+    return repeated
+     
 };
 
 // Do not edit below this line
