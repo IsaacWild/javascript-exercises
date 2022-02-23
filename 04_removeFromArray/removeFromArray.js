cdbbbbe3ee6d33142f6ac3cf09c611000fee200b
Isaac <isaacwildsecond@gmail.com>
2022-02-23 15:05:24 +0000

    added comment to code for self exlanation

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,25 +1,17 @@
 const removeFromArray = function() {
     const array = arguments[0];
-    //const toRemove = arguments[1];
     const arraylength = array.length;
-    //make an aray of items to be removed?
+    //make an aray of items to be removed
     let toRemove = [];
     for (let i = 1; i < arguments.length; i++){
         toRemove.push(arguments[i])
     }
-    //checking new to remove array
-    // console.log("The array: " + array);
-    // console.log("Array length: " + arraylength);
-    // console.log("To be removed array: " + toRemove);
-    // console.log("To be removed array length: " + toRemove.length);
-    
     for (let i = 0; i < arraylength; i++){
         console.log("Array item: " + array[i]);
         for(let j = 0; j < toRemove.length; j++){
-            // console.log("To be reomved item: " + toRemove[j]);
             if (toRemove[j] == array[i]){
-                // console.log("Spliced: " + array[i] + " from array");
                 array.splice(i, 1)
+                //after splice the array is shorter so need to set i back as to not miss the next array item that is now the current index value e.g. array[1,2,3,4] after '2' is removed '3' is now set to index [1] not index [2]
                 i--
             }
         }
