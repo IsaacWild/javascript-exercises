967e8c7e99d208a2a8e0f3d24c14796912a011ec
Isaac <isaacwildsecond@gmail.com>
2022-02-23 15:01:02 +0000

    Can take multiple arguments to be removed from the array successfully

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,30 @@
-const removeFromArray = function(array, toRemove) {
+const removeFromArray = function() {
+    const array = arguments[0];
+    //const toRemove = arguments[1];
     const arraylength = array.length;
+    //make an aray of items to be removed?
+    let toRemove = [];
+    for (let i = 1; i < arguments.length; i++){
+        toRemove.push(arguments[i])
+    }
+    //checking new to remove array
+    // console.log("The array: " + array);
+    // console.log("Array length: " + arraylength);
+    // console.log("To be removed array: " + toRemove);
+    // console.log("To be removed array length: " + toRemove.length);
+    
     for (let i = 0; i < arraylength; i++){
-        if (toRemove == array[i]){
-            array.splice(i, 1)
+        console.log("Array item: " + array[i]);
+        for(let j = 0; j < toRemove.length; j++){
+            // console.log("To be reomved item: " + toRemove[j]);
+            if (toRemove[j] == array[i]){
+                // console.log("Spliced: " + array[i] + " from array");
+                array.splice(i, 1)
+                i--
+            }
         }
     }
+    // console.log("After splice array: " + array)
     return array;
 };
 
