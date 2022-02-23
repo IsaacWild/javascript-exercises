878f84366c9e7b0f75630b029dfe8f894f3d7488
Isaac <isaacwildsecond@gmail.com>
2022-02-23 14:26:23 +0000

    Completed test 1 to remove item from a given array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, toRemove) {
+    const arraylength = array.length;
+    for (let i = 0; i < arraylength; i++){
+        if (toRemove == array[i]){
+            array.splice(i, 1)
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
