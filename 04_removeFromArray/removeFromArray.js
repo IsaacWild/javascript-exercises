2c5be64fcb558c7c34ed6e95b386835ef4ee0110
Isaac <isaacwildsecond@gmail.com>
2022-02-23 17:31:12 +0000

    removed only same type, passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -8,7 +8,7 @@ const removeFromArray = function() {
     }
     for (let i = 0; i < arraylength; i++){
         for(let j = 0; j < toRemove.length; j++){
-            if (toRemove[j] == array[i]){
+            if (toRemove[j] === array[i]){
                 array.splice(i, 1)
                 //after splice the array is shorter so need to set i back as to not miss the next array item that is now the current index value e.g. array[1,2,3,4] after '2' is removed '3' is now set to index [1] not index [2]
                 i--
