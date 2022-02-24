5e8eae5a40bbbb76496c7354fe46928b0b153c3d
Isaac <isaacwildsecond@gmail.com>
2022-02-24 12:28:03 +0000

    returns ERROR when given anything not a number, all test's complete!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(start, end) {
     let sum = 0;
-    if(start < 0 || end < 0){
+    if(start < 0 || end < 0 || typeof(start) !== "number" || typeof(end) !== "number"){
         return "ERROR";
     }
     else if (start < end){
