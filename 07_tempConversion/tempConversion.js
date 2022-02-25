795c55fe7bb25136d8bf70f163bb0329a0e36a39
Isaac <isaacwildsecond@gmail.com>
2022-02-25 12:03:53 +0000

    changed from decimal approximation of 9/5 and 5/9 to actual values and passed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const ftoc = function(fTemp) {
-  let cTemp = (fTemp - 32) *  0.5555555;
+  let cTemp = ((fTemp - 32) *  5)/9;
   cTemp = parseFloat(cTemp.toFixed(1));
   return cTemp;
 };
 
 const ctof = function(cTemp) {
-  let fTemp = cTemp * 0.5555555 + 32;
+  let fTemp = (cTemp * (9/5)) + 32;
   fTemp = parseFloat(fTemp.toFixed(1));
   return fTemp;
 };
