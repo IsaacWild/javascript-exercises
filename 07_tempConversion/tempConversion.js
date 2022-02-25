0eb5fed57585bba6dc43e02fc003123d32716e3e
Isaac <isaacwildsecond@gmail.com>
2022-02-25 11:47:57 +0000

    can convert basic c to f and f to C with 1 decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  let cTemp = (fTemp - 32) *  0.555;
+  cTemp.toFixed(1);
+  return cTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  let fTemp = cTemp * 0.555 + 32;
+  fTemp.toFixed(1);
+  return fTemp;
 };
 
 // Do not edit below this line
