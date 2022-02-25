58cdf3e14df6b256c4b49c8f2d5bbf6b14a429d1
Isaac <isaacwildsecond@gmail.com>
2022-02-25 11:54:57 +0000

    can convert f to c to 1 decimal and with negatives

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,12 @@
 const ftoc = function(fTemp) {
-  let cTemp = (fTemp - 32) *  0.555;
-  cTemp.toFixed(1);
+  let cTemp = (fTemp - 32) *  0.5555555;
+  cTemp = parseFloat(cTemp.toFixed(1));
   return cTemp;
 };
 
 const ctof = function(cTemp) {
-  let fTemp = cTemp * 0.555 + 32;
-  fTemp.toFixed(1);
+  let fTemp = cTemp * 0.5555555 + 32;
+  fTemp = parseFloat(fTemp.toFixed(1));
   return fTemp;
 };
 
