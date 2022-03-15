15fb8e3f6e24f36e085aabbd1ec2489da1c166be
Isaac <isaacwildsecond@gmail.com>
2022-03-15 16:03:27 +0000

    java calculator task completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let sum = 0;
+  
+  a.forEach(Element => sum+=(Element));
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(a) {
+  let sum = a[0];
+  let aLenght = a.length;
 
+  for (let i = 1; i < aLenght; i++){
+    sum *= a[i]
+  }
+
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a == 0){
+    return 1;
+  } else
+	return a * factorial(a-1);
 };
 
 // Do not edit below this line
