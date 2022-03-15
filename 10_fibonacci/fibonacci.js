8b25f93d80330073c0f6cf2f79bf64a477a96469
Isaac <isaacwildsecond@gmail.com>
2022-03-15 16:48:55 +0000

    completed all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibNum) {
+    let fibSeq = [1,1];
+    if (fibNum < 0){
+        return "OOPS";
+    }else
+    for (i=2; i<fibNum; i++){
+        let a = fibSeq[i - 2];
+        let b = fibSeq[i - 1];
+        fibSeq.push(a+b);
+    }
+    return fibSeq[fibNum - 1];
 };
 
 // Do not edit below this line
